f476b64499e0a2f1fe101d9d35fbe3dfb271d6ed
礒部洸生 <isobekouki@isobekouseinoMacBook-Pro.local>
2020-06-18 15:07:55 +0900

    [fix]cart_item end_user_id

--- db/migrate/20200314045550_create_cart_items.rb
+++ db/migrate/20200314045550_create_cart_items.rb
@@ -1,9 +1,9 @@
 class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_items do |t|
-      t.string :end_user_id
-      t.string :item_id
-      t.string :amount
+      t.integer :end_user_id
+      t.integer :item_id
+      t.integer :amount
 
       t.timestamps
     end
--- db/migrate/20200314053026_change_data_to_cart_item.rb
+++ db/migrate/20200314053026_change_data_to_cart_item.rb
@@ -1,8 +1,8 @@
 class ChangeDataToCartItem < ActiveRecord::Migration[5.2]
   def change
     # change_column :cart_items, :end_user_id, :integer
-    change_column :cart_items, :end_user_id, :integer, using: 'column_name::integer'
-    change_column :cart_items, :item_id, :integer
-    change_column :cart_items, :amount, :integer
+    # change_column :cart_items, :end_user_id, :integer, using: 'column_name::integer'
+    # change_column :cart_items, :item_id, :integer
+    # change_column :cart_items, :amount, :integer
   end
 end
